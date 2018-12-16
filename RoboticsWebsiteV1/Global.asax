dbed214d9c468aa90b5900a03843f76fff35cf20
ocampossoto <ocampossoto1@gmail.com>
2018-12-16 00:46:41 -0600

    Add project files.

---
+++ RoboticsWebsiteV1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RoboticsWebsiteV1.Global" Language="C#" %>
---
+++ RoboticsWebsiteV1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RoboticsWebsiteV1.Startup))]
+namespace RoboticsWebsiteV1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
